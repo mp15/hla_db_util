ca3bdf5b0f76cbf4b053745bbc97ceba33373e2c
Martin O. Pollard <mp15@sanger.ac.uk>
2016-01-27 13:45:19 +0000

    Make hla_exons_sep_fastq.pl do all exons and all genes

--- hla_exons_sep_fastq.pl
+++ hla_exons_sep_fastq.pl
@@ -32,9 +32,7 @@ while ( (my $seq = $stream->next_seq()) ) {
             foreach my $feature (@features) {
                 if ($feature->primary_tag eq  'exon') {
                     my @number = $feature->get_tag_values('number');
-                    if ($number[0] eq '2' || $number[0] eq '3' || $number[0] eq '4') {
-                         $hla{$allele_name}{$number[0]} = $feature->seq()->seq();
-                    }
+                    $hla{$allele_name}{$number[0]} = $feature->seq()->seq();
                 }
             }
        }
@@ -45,27 +43,10 @@ while ( (my $seq = $stream->next_seq()) ) {
 }
 print STDERR "There are $num_seq sequences in this file\n";
 foreach my $key (sort keys %hla) {
-    next if ($key !~ /^HLA-[A-C].*/);
-    print ">${key} exon2\n";
-    print $hla{$key}{2}."\n";
-    print ">${key} exon3\n";
-    print $hla{$key}{3}."\n";
-    if (exists $hla{$key}{4}) {
-        print ">${key} exon4\n";
-        print $hla{$key}{4}."\n";
+    foreach my $exon (sort keys $hla{$key}) {
+        print ">${key} exon$exon\n";
+        print $hla{$key}{$exon}."\n";
     }
 }
 
-foreach my $key (sort keys %hla) {
-    next if ($key !~ /^HLA-D(RB[1345]|[PQ][AB]1).*/);
-    next if ($key =~ /^HLA-DRB4\*03:01N/); # No exon 2 data
-    if (exists $hla{$key}{2}) {
-        print ">${key}\n";
-        print $hla{$key}{2};
-    }
-    if (exists $hla{$key}{3}) {
-        print ">${key} exon3\n";
-        print $hla{$key}{3}."\n";
-    }
-}
 print STDERR "Psuedo genes $psuedo_gene\n";
