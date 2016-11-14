1ec244f2f4ec8a1017a322b7f2e90deec48ee036
Martin O. Pollard <mp15@sanger.ac.uk>
2016-11-14 14:00:23 +0000

    Get g group converter to testing stage

--- g_group.pl
+++ g_group.pl
@@ -9,13 +9,25 @@ use warnings;
 my $ref = $ARGV[0];
 
 my $ref_fh;
+my %ref_in;
 open($ref_fh, "<", $ref) or die "Could not open reference $ref";
 
 while (<$ref_fh>) {
 	chomp;
-	split /;/;
+    next if $_ =~ /^#/;
+	my @a = split /;/;
+    if (scalar @a == 2) {
+        $ref_in{$a[0]}{$a[1]} = $a[1];
+    } else {
+        foreach my $b (split(/\//, $a[1])) {
+            $ref_in{$a[0]}{$b} = $a[2];
+        }
+    }
 }
 close $ref_fh;
 
-my $input_fh;
+use Data::Dump qw(dump);
+print dump(%ref_in);
+#my $input_fh;
+#open($input_fh, "<-") or die "Cannot open stdin";
 
